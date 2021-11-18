d5f69c38bc396af2123c479e0e55228fc59b82f3
Burt Karl Cabigas <bortsigan@users.noreply.github.com>
2021-11-18 14:03:42 +0800

    changes

--- backend/resources/views/users/posts/edit.blade.php
+++ backend/resources/views/users/posts/edit.blade.php
@@ -38,12 +38,10 @@
 		@enderror
 	</div>
 
-	<div class="mb-3">
-		<img src="{{ asset('/storage/images/' . $post->image) }}" class="card-img" /> 
-
-		@error('image')
-			<p class="text-danger">{{ $message }}</p>
-		@enderror
+	<div class="row mb-3">
+		<div class="col-6">
+			<img src="{{ asset('/storage/images/' . $post->image) }}" class="card-img" /> 
+		</div>
 	</div>
 
 	<div class="mb-3">
